99d768851eb6ebfa98399861775b6614c2246153
rdayok <r.dayok@native.semicolon.africa>
2023-11-25 16:37:57 +0100

    created the springboot project for gigstar backend

---
+++ src/main/java/com/rdi/gigstar/GigstarApplication.java
@@ -0,0 +1,13 @@
+package com.rdi.gigstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigstarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GigstarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rdi/gigstar/GigstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdi.gigstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigstarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
