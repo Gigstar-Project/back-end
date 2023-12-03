6d3d61c7d48bb7c1b9f62415be4aaff6b511779b
rdayok <r.dayok@native.semicolon.africa>
2023-12-03 02:48:19 +0100

    implemented decline booking request for geegstar

---
+++ src/main/java/com/rdi/geegstar/exceptions/BookingNotFoundException.java
@@ -0,0 +1,8 @@
+package com.rdi.geegstar.exceptions;
+
+public class BookingNotFoundException extends GeegStarException {
+    public BookingNotFoundException(String message) {
+        super(message);
+
+    }
+}
