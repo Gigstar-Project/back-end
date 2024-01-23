4d4b84314ae402b56ee54bec5edc7e4264a12c7b
rdayok <r.dayok@native.semicolon.africa>
2024-01-23 15:44:49 +0100

    implemented get booking by id end point with test

--- src/main/java/com/rdi/geegstar/data/repositories/CalenderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rdi.geegstar.data.repositories;
-
-import com.rdi.geegstar.data.models.Calendar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CalenderRepository extends JpaRepository<Calendar, Long> {
-}
