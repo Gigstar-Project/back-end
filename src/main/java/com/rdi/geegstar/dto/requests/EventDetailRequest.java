4f83d1ba0276d41da813b8a2cf0bbd5ddbf14d25
rdayok <r.dayok@native.semicolon.africa>
2024-02-09 16:07:51 +0100

    fixed bug in the talent request object which was stoping validation

--- src/main/java/com/rdi/geegstar/dto/requests/EventDetailRequest.java
+++ src/main/java/com/rdi/geegstar/dto/requests/EventDetailRequest.java
@@ -1,6 +1,9 @@
 package com.rdi.geegstar.dto.requests;
 
 import com.rdi.geegstar.enums.EventType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,8 +14,10 @@ import lombok.Setter;
 public class EventDetailRequest {
     private String eventName;
     @Pattern(regexp = "\\d{4}, \\d{2}, \\d{2}, \\d{2}, \\d{2}",
-            message = "Date and time must be in the format YYYY-MM-DD HH:mm. For example: 2023, 12, 24, 16, 30")
+            message = "Date and time must be in the format YYYY, MM, DD HH, mm. For example: 2023, 12, 24, 16, 30")
     private String eventDateAndTime;
     private AddressRequest eventAddress;
+    @NotBlank(message = "You need to select the type of event")
+    @Enumerated(EnumType.STRING)
     private EventType eventType;
 }
